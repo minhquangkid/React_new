cd891125849956647fb338a636e8c063f8d021e0
minh quang <minhquangkid@gmail.com>
2022-07-29 21:59:38 +0700

    xong lab4.2

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -9,13 +11,32 @@ const NewExpense = (props) => {
     };
     // console.log(expenseData);
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseDate={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseDate={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
+// (!isEditing) nghĩa là nếu isEditing là false thì hiện nút button , còn ngược lại thì hiện ExpenseForm
 // tạo 1 thuộc tính mới có tên là onSaveExpenseDate, nó là 1 hàm có tên là saveExpenseDataHandler dùng để nhận và lưu dữ liệu từ giao diện người dùng trong component ExpenseForm
 export default NewExpense;
