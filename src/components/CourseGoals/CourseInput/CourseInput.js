a9b6c3aad855e57015ab026ddf2efbe5c614d8cc
minh quang <minhquangkid@gmail.com>
2022-07-31 22:25:06 +0700

    using CSS Module

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,34 +1,34 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Button from "../../UI/Button/Button";
-import "./CourseInput.css";
+import styles from "./CourseInput.module.css";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
 
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${(props) => (props.invalid ? "red" : "black")};
-  }
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//     color: ${(props) => (props.invalid ? "red" : "black")};
+//   }
 
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${(props) => (props.inValid ? "yellow" : "#ccc")};
-    background: ${(props) => (props.invalid ? "blue" : "#ccc")};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid ${(props) => (props.inValid ? "yellow" : "#ccc")};
+//     background: ${(props) => (props.invalid ? "blue" : "#ccc")};
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
 
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+// `;
 // tạo 1 styled component cho thẻ div, nó sẽ có các thuộc tính css như  margin: 0.5rem 0;
 // nếu những thứ bên trong nó có chứa input, label thì nó sẽ sử dụng thuộc tính css là & input, & label cho input và label đó
 // tại border của input, ta sử dụng arrow function, với tham số đầu vào là props, nếu props.invalid là true thì sẽ dùng red
@@ -69,16 +69,20 @@ const CourseInput = (props) => {
   return (
     <form onSubmit={formSubmitHandler}>
       {/* <FormControl className={!isValid && "invalid"}> */}
-      <FormControl invalid={!isValid}>
-        {/* <div className={`form-control ${!isValid ? "invalid" : ""}`}> */}
+      {/* <FormControl invalid={!isValid}> */}
+      <div
+        className={`${styles["form-control"]} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-        {/* </div> */}
-      </FormControl>
+      </div>
+      {/* </FormControl> */}
       <Button type="submit">Add Goal</Button>
     </form>
   );
 };
+// dùng dấu [] trong styles["form-control"] vì giữa từ form và control có dấu "-" nên ko dùng styles.form-control được
+
 // sử dụng  <div className={`form-control ${!isValid ? "invalid" : ""}`}> để dùng class .form-control.invalid đã tạo ở file css cho trường hợp người dùng nhập rỗng, và nó sẽ ko được dùng khi người dùng nhập khác rỗng
 
 // nếu chúng ta ko sử dụng toán tử điều kiện thì ta có thể sử dụng cách khác là dùng props cho FormControl. Tạo 1 thuộc tính là invalid nhận tham số là !isValid, nếu !isValid là true thì nó sẽ kích hoạt thuộc tính invalid, và ngược lại sẽ ko kích hoạt
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,43 +1,45 @@
 import React from "react";
 import styled from "styled-components";
-// import './Button.css';
+import styles from "./Button.module.css";
+// mặc dù import cụ thể 1 file css nhưng ta có thể sử dụng các class từ các file css khác mà ko cần import, để tránh gây nhầm lẫn css ta sử dụng css modules
+// dùng import classes from "./Button.css"; cũng được
 
 // tạo component Button nhưng sử dụng package styled
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
 
-  @media (min-width: 768px) {
-    width: auto;
-  }
-`;
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
+// `;
 // tạo 1 styled component tên là Button cho thẻ button
 // sử dụng responsive với @media (min-width: 768px) tức là các thiết bị lớn hơn 768px sẽ được thu lại nút button, còn thiết bị nhỏ hơn 768px như di động sẽ hiện width=100%
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
-
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
+// sử dụng css module cho nút button bằng lệnh className={styles.button}
 export default Button;
 // dùng styled-components để xác định rõ định dạng css cho 1 đối tượng, nhằm giúp ko nhầm lẫn với các đối tượng khác khi dùng theo class thông thường
