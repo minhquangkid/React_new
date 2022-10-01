66782468869cf177fb9fdf830dceb326ace82076
minh quang <minhquangkid@gmail.com>
2022-10-01 22:28:56 +0700

    bug lab19.1

---
+++ build/precache-manifest.041884d42c552bb7d3fb0cdfcb22d45a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26790282c1b67962a0d3d40f31311051",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23b63a45276698348698",
+    "url": "/static/css/main.f8669f4a.chunk.css"
+  },
+  {
+    "revision": "8e2657c5d728e4d7f0bd",
+    "url": "/static/js/2.ea3cfe6c.chunk.js"
+  },
+  {
+    "revision": "60e568a4a8c64c3874edcdbfaeeb3d5d",
+    "url": "/static/js/2.ea3cfe6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23b63a45276698348698",
+    "url": "/static/js/main.997f3d4e.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.997f3d4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "260705052eb5a5c8bd41",
+    "url": "/static/js/runtime-main.d3cf5db1.js"
+  }
+]);
--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -1,16 +1,11 @@
+import { Link } from "react-router-dom";
+
 import React, { useEffect, useState } from "react";
 import classes from "./navbar.module.css";
 
 const Navbar = () => {
   const [scrolled, setScrolled] = useState(false);
   // const [test, setTest] = useState("");
-
-  const goToSearch = () => {
-    window.location.replace("http://localhost:3000/search");
-  };
-  const goToHome = () => {
-    window.location.replace("http://localhost:3000/");
-  };
 
   useEffect(() => {
     console.log("đã tạo");
@@ -45,25 +40,27 @@ const Navbar = () => {
       <div
         className={` ${classes.navbarFrame} ${scrolled && classes.transfer}`}
       >
-        <h1 onClick={goToHome} className={classes.chu}>
-          Movie App
-        </h1>
-        <span className={classes.searchIcon} onClick={goToSearch}>
-          <svg
-            className="svg-inline--fa fa-search fa-w-16 "
-            fill="#ccc"
-            aria-hidden="true"
-            data-prefix="fas"
-            data-icon="search"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            width="30px"
-            height="30px"
-          >
-            <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
-          </svg>
-        </span>
+        <Link to={"/"}>
+          <h1 className={classes.chu}>Movie App</h1>
+        </Link>
+        <Link to={"/search"}>
+          <span className={classes.searchIcon}>
+            <svg
+              className="svg-inline--fa fa-search fa-w-16 "
+              fill="#ccc"
+              aria-hidden="true"
+              data-prefix="fas"
+              data-icon="search"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 512 512"
+              width="30px"
+              height="30px"
+            >
+              <path d="M505 442.7L405.3 343c-4.5-4.5-10.6-7-17-7H372c27.6-35.3 44-79.7 44-128C416 93.1 322.9 0 208 0S0 93.1 0 208s93.1 208 208 208c48.3 0 92.7-16.4 128-44v16.3c0 6.4 2.5 12.5 7 17l99.7 99.7c9.4 9.4 24.6 9.4 33.9 0l28.3-28.3c9.4-9.4 9.4-24.6.1-34zM208 336c-70.7 0-128-57.2-128-128 0-70.7 57.2-128 128-128 70.7 0 128 57.2 128 128 0 70.7-57.2 128-128 128z"></path>
+            </svg>
+          </span>
+        </Link>
       </div>
     </React.Fragment>
   );
