16e4d05825054dae05da136494344c5085967d0d
quang <minhquangkid@gmail.com>
2024-10-13 11:27:18 +0700

    remove bind

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -26,8 +26,8 @@ const Cart = (props) => {
           name={item.name}
           amount={item.amount}
           price={item.price}
-          onRemove={cartItemRemoveHandler.bind(null, item.id)}
-          onAdd={cartItemAddHandler.bind(null, item)}
+          onRemove={() => cartItemRemoveHandler(item.id)}
+          onAdd={() => cartItemAddHandler(item)}
         />
       ))}
     </ul>
